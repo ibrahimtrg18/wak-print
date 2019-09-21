98949cd0e86cbda588783abceaddf52a622f23ea
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2019-09-21 09:03:41 +0700

    add json/cors

--- server/app.js
+++ server/app.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 const PORT = process.env.PORT || 4000;
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
+app.use(cors());
 
 app.use("/", require("./routes/users"));
 
