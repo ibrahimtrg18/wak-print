046163ac1dc54fa0c31e790e48320b6b4129d44e
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2019-09-21 13:07:32 +0700

    fix database

--- server/config/db.js
+++ server/config/db.js
@@ -6,7 +6,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    database: "db_print_online"
+    database: "wakprint"
 })
 
 // Connect to Mysql
