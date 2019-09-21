d36fca0bd64a400bcc56378b74991d3bfe948803
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2019-09-21 09:01:03 +0700

    add database

---
+++ server/config/db.js
@@ -0,0 +1,22 @@
+
+const mysql = require("mysql");
+
+// Create Connection to Mysql
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "db_print_online"
+})
+
+// Connect to Mysql
+connection.connect((err) => {
+    if (err) {
+        console.log("fail connection to database");
+        return;
+    }
+
+    console.log("success connection to database");
+})
+
+module.exports = connection;
