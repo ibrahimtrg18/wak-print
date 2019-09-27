54bc8e94515d968d33d043cc57c0a8395c9ddcf6
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2019-09-27 09:12:56 +0700

    fix naming var and object

--- server/routes/pesanan.js
+++ server/routes/pesanan.js
@@ -39,49 +39,58 @@ isPrintType = (file, cb) => {
 
 router.post("/", upload.single("pesanan"), (req, res) => {
     let errors = {};
-    const file = req.file
     const {
-        id_user,
-        id_wak_print,
-        jumlah_halaman_pesanan,
-        jumlah_rangkap_pesanan,
-        timbal_balik_pesanan,
-        orientasi_halaman_pesanan,
-        jenis_kertas_pesanan,
-        total_harga_pesanan,
-        metode_pengambilan_pesanan,
-        metode_pembayaran_pesanan,
-        status_pesanan,
-        status_pembayaran
+        idUser,
+        idWakPrint,
+        jumlahHalamanPesanan,
+        jumlahRangkapPesanan,
+        timbalBalikPesanan,
+        orientasiHalamanPesanan,
+        jenisKertasPesanan,
+        totalHargaPesanan,
+        metodePengambilanPesanan,
+        metodePembayaranPesanan,
+        statusPesanan,
+        statusPembayaran
     } = req.body;
 
-    if (!file) {
+    if (!req.file) {
         errors = { message: "Please Put your File" }
         return res.json({ errors })
     }
     // res.json(file.filename)
-    const dokumen_user = file.filename
+    const dokumenUser = req.file.filename
 
-    connection.query("INSERT INTO pesanan SET ?",
-    {
-        id_user,
-        id_wak_print,
-        dokumen_user,
-        jumlah_halaman_pesanan,
-        jumlah_rangkap_pesanan,
-        timbal_balik_pesanan,
-        orientasi_halaman_pesanan,
-        jenis_kertas_pesanan,
-        total_harga_pesanan,
-        metode_pengambilan_pesanan,
-        metode_pembayaran_pesanan,
-        status_pesanan,
-        status_pembayaran        
-    },(err,results)=>{
-        if(err){
+    connection.query("INSERT INTO pesanan SET ?", {
+        id_user : idUser,
+        id_wak_print : idWakPrint,
+        dokumen_user : dokumenUser,
+        jumlah_halaman_pesanan : jumlahHalamanPesanan,
+        jumlah_rangkap_pesanan : jumlahRangkapPesanan,
+        timbal_balik_pesanan : timbalBalikPesanan,
+        orientasi_halaman_pesanan : orientasiHalamanPesanan,
+        jenis_kertas_pesanan : jenisKertasPesanan,
+        total_harga_pesanan : totalHargaPesanan,
+        metode_pengambilan_pesanan :metodePengambilanPesanan,
+        metode_pembayaran_pesanan : metodePembayaranPesanan,
+        status_pesanan : statusPesanan,
+        status_pembayaran : statusPembayaran
+    }, (err, results) => {
+        if (err) {
             return console.log(err);
-        }else{
+        } else {
             return console.log(results);
+        }
+    })
+})
+
+router.get("/:idPesanan/", (req, res) => {
+    console.log(req.params.idPesanan)
+    connection.query("SELECT * FROM pesanan WHERE id_pesanan = ?", req.params.idPesanan, (err, results) => {
+        if (err) {
+            return console.log(err);
+        } else {
+            return res.json(results)
         }
     })
 })
