8fd822c68ccb5542cd7f7f914040e6392e1cf518
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2019-09-27 12:18:29 +0700

    fix body user

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,52 +3,85 @@ const router = express.Router();
 
 const connection = require("../config/db")
 
-router.get("/register", (req, res) => {
-    res.send("register")
-})
-
 router.post("/register", (req, res) => {
-    console.log(req.body);
-    let { email_user, password_user, password_confirm_user, nama_user, nomor_hp_user, alamat_user, foto_user } = req.body;
+    const {
+        emailUser,
+        passwordUser,
+        passwordConfirmUser,
+        namaUser,
+        nomorHpUser,
+        alamatUser,
+        fotoUser
+    } = req.body;
     let errors;
 
     // Cek Panjang Password
-    if (password_user && password_user.length < 6) {
-        errors = { message: "Password harus lebih dari 6 karakter" }
+    if (passwordUser && passwordUser.length < 6) {
+        errors = {
+            message: "Password harus lebih dari 6 karakter"
+        }
     }
 
     // Cek Password Sama
-    if (password_user !== password_confirm_user) {
-        errors = { message: "Password tidak sama!" }
+    if (passwordUser !== passwordConfirmUser) {
+        errors = {
+            message: "Password tidak sama!"
+        }
     }
 
     // Cek Semua terisi
-    if (!email_user || !password_user || !password_confirm_user || !nama_user || !nomor_hp_user || !alamat_user) {
-        errors = { message: "Silakan isi semua-nya!" }
+    if (!emailUser || !passwordUser || !passwordConfirmUser || !namaUser || !nomorHpUser || !alamatUser) {
+        errors = {
+            message: "Silakan isi semua-nya!"
+        }
     }
-
 
     // Cek Error
     if (errors) {
-        // Jika ada respon error ke client
-        res.json({ errors });
+        // Jika ada error dari client
+        res.json({
+            errors
+        });
     } else {
-        // Memasukan kedalam table_user
-        connection.query("SELECT * FROM user WHERE email_user=?", email_user, (err, results) => {
+        // Memasukan kedalam tableUser
+        connection.query("SELECT * FROM user WHERE email_user = ?", emailUser, (err, results) => {
             if (err) {
-                console.log(err);
-                errors = { message: err }
+                errors = {
+                    message: err
+                }
                 return res.json(errors)
             } else if (results && results.length > 0) {
-                errors = { message: "Email sudah terdaftar!" }
-                return res.json({ errors })
+                errors = {
+                    message: "Email sudah terdaftar!"
+                }
+                return res.json({
+                    errors
+                })
             } else {
-                connection.query("INSERT INTO user SET ?", { email_user, password_user, nama_user, nomor_hp_user, alamat_user, foto_user }, (err) => {
+                connection.query("INSERT INTO user SET ?", {
+                    email_user: emailUser,
+                    password_user: passwordUser,
+                    nama_user: namaUser,
+                    nomor_hp_user: nomorHpUser,
+                    alamat_user: alamatUser,
+                    foto_user: fotoUser
+                }, (err) => {
                     if (err) {
-                        errors = { message: err }
+                        errors = {
+                            message: err
+                        }
                         return res.json(errors)
                     } else {
-                        return res.json({ data: { email_user, password_user, nama_user, nomor_hp_user, alamat_user, foto_user } })
+                        return res.json({
+                            data: {
+                                emailUser,
+                                passwordUser,
+                                namaUser,
+                                nomorHpUser,
+                                alamatUser,
+                                fotoUser
+                            }
+                        })
                     }
                 });
             }
@@ -56,28 +89,34 @@ router.post("/register", (req, res) => {
     }
 });
 
-
-router.get("/login", (req, res) => {
-    res.send("login")
-});
-
 router.post("/login", (req, res) => {
     console.log(req.body);
-    const { email, password } = req.body;
+    const {
+        emailUser,
+        passwordUser
+    } = req.body;
     let errors = {};
 
-    // Cek Email dan Password sama dengan salah satu row didalam table_user
-    connection.query("SELECT email,password FROM user WHERE email=? AND password=?", [email, password], (err, results) => {
+    // Cek Email dan Password sama dengan salah satu row didalam tableUser
+    connection.query("SELECT email_user, password_user FROM user WHERE email_user=? AND password_user=?", [emailUser, passwordUser], (err, results) => {
         if (err) {
             return console.log(err);
         } else {
             if (results && results.length > 0) {
                 // Jika cocok
-                return res.json({ data: { results } });
+                return res.json({
+                    data: {
+                        results
+                    }
+                });
             } else {
                 // Jika tidak cocok
-                errors = { message: "Check kembali email dan password!" }
-                return res.json({ errors });
+                errors = {
+                    message: "Check kembali email dan password!"
+                }
+                return res.json({
+                    errors
+                });
             }
         }
     });
