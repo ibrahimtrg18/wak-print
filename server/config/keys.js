38970b9790b964a451e0d0607cd81ebd4f54b0ca
Ibrahim Tarigan <41787706+t33rg3@users.noreply.github.com>
2019-10-03 21:27:11 +0700

    Delete keys.js

--- server/config/keys.js
+++
@@ -1,9 +0,0 @@
-// add this to .gitignore
-
-module.exports = {
-    google: {
-        clientID: "539817468757-ucl50eetebdosft65bh2ku8seetacu27.apps.googleusercontent.com",
-        clientSecret: "vgFD3GR-XUoyftjRfyznDI8w"
-    }
-    
-}
