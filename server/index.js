c64273f708ce709e766542f2bcd15ab2d8e17846
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2019-10-04 21:19:49 +0700

    some change

--- server/index.js
+++ server/index.js
@@ -8,7 +8,10 @@ app.use(express.urlencoded({
     extended: false
 }))
 
-app.use(express.json());
+app.use(express.json(), {
+    urlencoded: true
+});
+
 app.use(cors());
 
 app.use("/user", require("./routes/user")); //user
