c240fb315c1ff70520ab6d7ff80bde216b68887a
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2019-10-10 13:44:52 +0700

    change user response json

--- server/routes/user.js
+++ server/routes/user.js
@@ -7,126 +7,118 @@ const connection = require("../config/db")
 router.post("/register", (req, res) => {
     console.log(req.body);
     const {
-        emailUser,
-        passwordUser,
-        passwordConfirmUser,
-        namaUser,
-        nomorHpUser,
-        alamatUser,
-        fotoUser
+        email_user,
+        password_user,
+        password_confirm_user,
+        nama_lengkap_user,
+        nomor_hp_user,
+        alamat_user,
+        foto_user
     } = req.body;
-    let errors;
+
+    // Cek Semua terisi
+    if (!email_user || !password_user || !password_confirm_user || !nama_lengkap_user || !nomor_hp_user || !alamat_user) {
+        return res.json({
+            status: "fail",
+            message: "tolong isi data anda!"
+        })
+    }
 
     // Cek Panjang Password
-    if (passwordUser && passwordUser.length < 6) {
-        errors = {
-            message: "Password harus lebih dari 6 karakter"
-        }
+    if (password_user && password_user.length < 6) {
+        return res.json({
+            status: "fail",
+            message: "password harus lebih dari 6!"
+        })
     }
 
     // Cek Password Sama
-    if (passwordUser !== passwordConfirmUser) {
-        errors = {
-            message: "Password tidak sama!"
-        }
+    if (password_user !== password_confirm_user) {
+        return res.json({
+            status: "fail",
+            message: "password tidak sama!"
+        })
     }
 
-    // Cek Semua terisi
-    if (!emailUser || !passwordUser || !passwordConfirmUser || !namaUser || !nomorHpUser || !alamatUser) {
-        errors = {
-            message: "Silakan isi semua-nya!"
-        }
-    }
-
-    // Cek Error
-    if (errors) {
-        // Jika ada error dari client
-        res.json({
-            errors
-        });
-    } else {
-        // Memasukan kedalam tableUser
-        connection.query("SELECT * FROM user WHERE email_user = ?", emailUser, (err, results) => {
-            if (err) {
-                errors = {
-                    message: err
-                }
-                return res.json(errors)
-            } else if (results && results.length > 0) {
-                errors = {
-                    message: "Email sudah terdaftar!"
-                }
-                return res.json({
-                    errors
-                })
-            } else {
-                connection.query("INSERT INTO user SET ?", {
-                    email_user: emailUser,
-                    password_user: passwordUser,
-                    nama_user: namaUser,
-                    nomor_hp_user: nomorHpUser,
-                    alamat_user: alamatUser,
-                    foto_user: fotoUser
-                }, (err) => {
-                    if (err) {
-                        errors = {
-                            message: err
+    // Memasukan kedalam tableUser
+    connection.query("SELECT * FROM user WHERE email_user = ?", email_user, (err, results) => {
+        if (err) {
+            return res.json({
+                status: "error",
+                message: "ada kesalah didalam database"
+            })
+        } else if (results && results.length > 0) {
+            return res.json({
+                status: "fail",
+                message: "email anda sudah terdaftar!"
+            })
+        } else {
+            connection.query("INSERT INTO user SET ?", {
+                email_user,
+                password_user,
+                nama_lengkap_user,
+                nomor_hp_user,
+                alamat_user,
+                foto_user
+            }, (err) => {
+                if (err) {
+                    console.log(err)
+                    return res.json({
+                        status: "error",
+                        message: "ada kesalah didalam database2"
+                    })
+                } else {
+                    return res.json({
+                        status: "success",
+                        data: {
+                            email_user,
+                            password_user,
+                            nama_lengkap_user,
+                            nomor_hp_user,
+                            alamat_user,
+                            foto_user
                         }
-                        return res.json(errors)
-                    } else {
-                        return res.json({
-                            data: {
-                                emailUser,
-                                passwordUser,
-                                namaUser,
-                                nomorHpUser,
-                                alamatUser,
-                                fotoUser
-                            }
-                        })
-                    }
-                });
-            }
-        });
-    }
+                    })
+                }
+            });
+        }
+    });
 });
 
 router.post("/login", (req, res) => {
     console.log(req.body);
     const {
-        emailUser,
-        passwordUser
+        email_user,
+        password_user
     } = req.body;
-    let errors = {};
 
-    if (!emailUser || !passwordUser) {
-        errors = {
-            message: "Tolong isi email dan password anda!"
-        }
+    if (!email_user || !password_user) {
         return res.json({
-            errors
+            status: "error",
+            message: "Tolong isi email dan password anda!"
         });
     }
 
     // Cek Email dan Password sama dengan salah satu row didalam tableUser
-    connection.query("SELECT * FROM user WHERE email_user=? AND password_user=?", [emailUser, passwordUser], (err, results) => {
+    connection.query("SELECT * FROM user WHERE email_user=? AND password_user=?", [email_user, password_user], (err, results) => {
         if (err) {
             return console.log(err);
         } else {
             if (results && results.length > 0) {
                 // Jika cocok
                 return res.json({
+                    status: "success",
                     data: {
                         results
                     }
                 });
             } else {
                 // Jika tidak cocok
-                errors = {
-                    message: "Check kembali email dan password!"
-                }
                 return res.json({
-                    errors
+                    status: "fail",
+                    data: {
+                        message: "Email dan Password salah!"
+                    }
                 });
             }
         }
