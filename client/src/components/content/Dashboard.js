83560439bec1a20a5d4608a9137dd37b4d61b699
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2019-10-12 07:06:54 +0700

    add some ui

--- client/src/components/content/Dashboard.js
+++ client/src/components/content/Dashboard.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid'
+import Table from '@material-ui/core/Table'
+import TableCell from '@material-ui/core/TableCell'
+import TableHead from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
+import TableBody from '@material-ui/core/TableBody'
+
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 
 import Sider from '../layout/Sider';
 import Navbar from '../layout/Navbar';
+import { Paper, Box, Divider } from '@material-ui/core';
 
 const drawerWidth = 240;
 
@@ -37,14 +45,34 @@ const useStyles = makeStyles(theme => ({
     flexGrow: 1,
     padding: theme.spacing(3),
   },
+  rootPesanan: {
+    width: '100%',
+    marginTop: theme.spacing(1),
+    overflowX: 'auto',
+  },
+  tablePesanan: {
+    minWidth: 450,
+  },
 }));
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
 
 function Dashboard(props) {
   const { container } = props;
   const classes = useStyles();
   const theme = useTheme();
   const [mobileOpen, setMobileOpen] = React.useState(false);
-
+  console.log(createData('Frozen yoghurt', 159, 6.0, 24, 4.0))
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
   };
@@ -55,29 +83,76 @@ function Dashboard(props) {
       <Sider mobileOpen={mobileOpen} handleDrawerToggle={() => handleDrawerToggle()}></Sider>
       <main className={classes.content}>
         <div className={classes.toolbar} />
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-          ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
-          facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-          gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
-          donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-          Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
-          imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
-          arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-          donec massa sapien faucibus et molestie ac.
-        </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
-          facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
-          tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
-          consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
-          vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
-          hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
-          tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
-          nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
-          accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
+        <Grid container spacing={3}>
+          <Grid item xs={6} sm={3}>
+            <Paper>
+              <Typography variant="h6">
+                E-Wallet
+              </Typography>
+              <Typography variant="h6">
+                E-Wallet
+              </Typography>
+            </Paper>
+          </Grid>
+          <Grid item xs={6} sm={3}>
+            <Paper>
+              <Typography variant="h6">
+                E-Wallet
+              </Typography>
+              <Typography variant="h6">
+                E-Wallet
+              </Typography>
+            </Paper>
+          </Grid>
+          <Grid item xs={6} sm={3}>
+            <Paper>
+              <Typography variant="h6">
+                E-Wallet
+              </Typography>
+              <Typography variant="h6">
+                E-Wallet
+              </Typography>
+            </Paper>
+          </Grid>
+          <Grid item xs={6} sm={3}>
+            <Paper>
+              <Typography variant="h6">
+                E-Wallet
+              </Typography>
+              <Typography variant="h6">
+                E-Wallet
+              </Typography>
+            </Paper>
+          </Grid>
+          <Grid item xs={12} sm={12}>
+            <Divider />
+            <Typography variant="h6" component="h6">
+              Pesanan
+              </Typography>
+            <Paper className={classes.rootPesanan}>
+              <Table className={classes.tablePesanan}>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Name</TableCell>
+                    <TableCell align="right">Jumlah Halaman</TableCell>
+                    <TableCell align="right">Jumlah Rangkap</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {rows.map(row => (
+                    <TableRow key={row.name}>
+                      <TableCell component="th" scope="row">
+                        {row.name}
+                      </TableCell>
+                      <TableCell align="right">{row.calories}</TableCell>
+                      <TableCell align="right">{row.fat}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </Paper>
+          </Grid>
+        </Grid>
       </main>
     </div>
   );
