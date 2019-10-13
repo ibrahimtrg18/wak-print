8a5c5763f6cedf586405c6c416892a7ff94c7b1c
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2019-10-13 11:33:31 +0700

    add state to localstorage and loadstorage to state

--- client/src/index.js
+++ client/src/index.js
@@ -10,7 +10,37 @@ import rootReducer from './components/redux/reducers/rootReducer'
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core';
 import purple from '@material-ui/core/colors/purple';
 
-const store = createStore(rootReducer)
+const saveToLocalStorage = (state) => {
+    try {
+        const serializedState = JSON.stringify(state)
+        localStorage.setItem('state', serializedState)
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+const loadFromLocalStorage = () =>{
+    try{
+        const serializedState = localStorage.getItem('state')
+        if(serializedState === null){
+            return undefined
+        }
+        return JSON.parse(serializedState)
+    }catch(e){
+        console.log(e)
+        return undefined
+    }
+}
+
+const persistedState = loadFromLocalStorage()
+
+const store = createStore(
+    rootReducer,
+    persistedState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+store.subscribe(() =>saveToLocalStorage(store.getState()))
 
 const theme = createMuiTheme({
     palette: {
