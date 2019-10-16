79f7ff8c8c61347db478684e3c50213a2af29078
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2019-10-16 18:04:09 +0700

    add register component

--- client_wakprint/src/App.js
+++ client_wakprint/src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 import Login from './components/Login'
+import Register from './components/Register';
 
 function App() {
   return (
     <div className="App" style={{margin: 0}}>
       <Router>
         <header className="App-header">
-          <Route path="/" component={Login}></Route>
+          <Route path="/register" component={Register}></Route>
+          <Route path="/" exact component={Login}></Route>
         </header>
       </Router>
     </div>
