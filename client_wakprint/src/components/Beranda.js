eb4fdc5966720ae2ba94e7bf11d2a5be585a23e3
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2019-10-18 13:16:00 +0700

    add Beranda components

---
+++ client_wakprint/src/components/Beranda.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Beranda = () => {
+  return (
+    <div className="bg-gray-100" style={{ height: "100%", minHeight: "100vh" }}>
+      <Navbar></Navbar>
+    </div>
+  )
+}
+
+export default Beranda
