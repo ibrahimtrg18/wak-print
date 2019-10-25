af036b98f7e05c34b1e293a3ceb779184896092d
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2019-10-25 13:43:43 +0700

    add redux

--- client_wakprint/src/index.js
+++ client_wakprint/src/index.js
@@ -4,7 +4,49 @@ import App from './App';
 import './index.css'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './redux/reducers/rootReducer'
+
+const saveToLocalStorage = (state) => {
+  try {
+    const serializedState = JSON.stringify(state)
+    localStorage.setItem('state', serializedState)
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+const loadFromLocalStorage = () => {
+  try {
+    const serializedState = localStorage.getItem('state')
+    if (serializedState === null) {
+      return undefined
+    }
+    return JSON.parse(serializedState)
+  } catch (e) {
+    console.log(e)
+    return undefined
+  }
+}
+
+const persistedState = loadFromLocalStorage()
+
+const store = createStore(
+  rootReducer,
+  persistedState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+store.subscribe(() => saveToLocalStorage(store.getState()))
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
