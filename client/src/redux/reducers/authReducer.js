370fac11a65aba3936bece667df9d346d649c964
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2019-11-08 09:57:09 +0700

    fix object data to state to props

--- client/src/redux/reducers/authReducer.js
+++ client/src/redux/reducers/authReducer.js
@@ -1,30 +1,27 @@
 import { AUTH_REQUEST, AUTH_SUCCESS, AUTH_FAILURE } from '../actions/authActions'
 
 const initialState = {
-  data: null,
-  isLoading: true,
+    data: null,
+    isLoading: true,
 };
 
 export const authReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case AUTH_REQUEST:
-      return {
-        ...state,
-        isLoading: true,
-      }
-    case AUTH_SUCCESS:
-      return {
-        ...state,
-        data: action.payload,
-        isLoading: false,
-      }
-    case AUTH_FAILURE:
-      return {
-        ...state,
-        message: "hello",
-        isLoading: false,
-      }
-    default:
-      return state
-  }
+    switch (action.type) {
+        case AUTH_REQUEST:
+            return {
+                isLoading: true,
+            }
+        case AUTH_SUCCESS:
+            return {
+                data: action.payload,
+                isLoading: false,
+            }
+        case AUTH_FAILURE:
+            return {
+                message: "Fail",
+                isLoading: false,
+            }
+        default:
+            return state
+    }
 }
