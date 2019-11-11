d9696d654566994a9b49956e2e02978de4b17dbd
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2019-11-11 21:12:45 +0700

    BIG FUCKING CHANGE

--- client/src/components/Order.js
+++ client/src/components/Order.js
@@ -1,8 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { connect } from "react-redux";
 import Navbar from './Navbar';
+import { orderData } from '../redux/actions/orderActions';
 
 const Order = (props) => {
+
+	const [partner, setPartner] = useState(props.auth.data);
+	const [orders, setOrders] = useState(props.order.data);
+	const [message, setMessage] = useState(null);
 
 	useEffect(() => {
 		document.title = "Order"
@@ -11,15 +16,68 @@ const Order = (props) => {
 	useEffect(() => {
 		if (!props.auth.data) {
 			props.history.push("/login");
+		} else if (partner && partner.id) {
+			props.orderData(partner.id)
 		}
 	}, [props.auth.data])
 
-	if (props.auth) {
+
+	console.log(orders)
+	if (props.auth.data) {
 		return (
-			<div className="bg-gray-100 h-screen">
+			<div className="bg-grayBg h-screen">
 				<Navbar goTo={"LogOut"}></Navbar>
-				<div className="sm:flex sm:pt-40 pt-48">
-					Order
+				<div className="sm:pt-32 pt-40 px-8">
+					<div className="text-3xl ml-2">Order</div>
+				</div>
+				<div className="flex flex-wrap px-8">
+					{orders.map(order => {
+						return (
+							<div className="w-full sm:w-1/2 md:w-1/3 p-1">
+								<div className="rounded shadow bg-white p-4" key={order.id}>
+									<div className="pt-2 pb-2">
+										<div className="">
+											<div className="text-grayText text-sm">Nama</div>
+											<div className="text-black font-medium text-base">{order.full_name}</div>
+										</div>
+										<div className="">
+											<div className="text-grayText text-sm">Nomor Telepon</div>
+											<div className="text-black font-medium text-base">{order.phone_number}</div>
+										</div>
+									</div>
+									<div className="text-black text-base font-semibold border-grayBorder border-b-2">Pengaturan File</div>
+									<div className="flex pb-2">
+										<div className="w-1/2">
+											<div className="text-grayText text-xs uppercase">Halaman</div>
+											<div className="border-grayBorder border-2 rounded px-2 inline">
+												{order.pages}
+											</div>
+										</div>
+										<div className="w-1/2">
+											<div className="text-grayText text-xs uppercase">Jumlah Rangkap</div>
+											<div className="border-grayBorder border-2 rounded px-2 inline">
+												{order.copies}
+											</div>
+										</div>
+									</div>
+									<div className="flex">
+										<div className="w-1/2 pb-2">
+											<div className="text-grayText text-xs uppercase">Product yang diminta</div>
+											<div className="border-grayBorder border-2 rounded px-2 inline">
+												{order.product_name}
+											</div>
+										</div>
+										<div className="w-1/2 pb-2">
+											<div className="text-grayText text-xs uppercase">Download File</div>
+											<div className="border-primary border-2 bg-primary text-white rounded px-2 inline cursor-pointer">
+												Download
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						)
+					})}
 				</div>
 			</div>
 		)
@@ -30,8 +88,15 @@ const Order = (props) => {
 
 const mapStateToProps = (state) => {
 	return {
-		auth: state.auth
+		auth: state.auth,
+		order: state.order
 	}
 }
 
-export default connect(mapStateToProps)(Order)
+const mapDispatchToProps = (dispatch) => {
+	return {
+		orderData: (partnerId) => { dispatch(orderData(partnerId)) }
+	}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Order)
