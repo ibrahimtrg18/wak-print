51995dfe4f2494462aef4ae823e2c2d8862a16fa
Ibrahim <ibrahimtarigan@gmail.com>
2019-12-06 17:55:33 +0700

    change format

--- client/src/components/Order.js
+++ client/src/components/Order.js
@@ -4,113 +4,112 @@ import Navbar from './Navbar';
 import { getOrders, resetOrders } from '../redux/actions/orderActions';
 
 const Order = (props) => {
+  const [message, setMessage] = useState(null);
+  const [orders, setOrders] = useState([]);
 
-	const [message, setMessage] = useState(null);
-	const [orders, setOrders] = useState([]);
+  useEffect(() => {
+    document.title = "Order"
+    props.resetOrders()
+  }, [])
 
-	useEffect(() => {
-		document.title = "Order"
-		props.resetOrders()
-	}, [])
+  useEffect(() => {
+    if (!props.auth.data) {
+      props.history.push("/login");
+    } else {
+      props.getOrders(props.auth.data.id)
+    }
+  }, [props.auth])
 
-	useEffect(() => {
-		if (!props.auth.data) {
-			props.history.push("/login");
-		} else {
-			props.getOrders(props.auth.data.id)
-		}
-	}, [props.auth.data])
+  useEffect(() => {
+    setMessage(props.order.message)
+    setOrders(props.order.data)
+  }, [props.order])
 
-	useEffect(() => {
-		setMessage(props.order.message)
-		setOrders(props.order.data)
-	}, [props.order])
-
-	console.log(orders)
-	if (props.auth.data) {
-		return (
-			<div className="bg-bg h-screen">
-				<Navbar goTo={"LogOut"}></Navbar>
-				<div className="sm:pt-32 pt-40 px-8">
-					<div className="text-3xl ml-2">Order</div>
-				</div>
-				<div className="flex flex-wrap px-8">
-					{!message ? orders && orders.length > 0 ? orders && orders.map(order => {
-						return (
-							<div className="w-full sm:w-1/2 md:w-1/3 p-1" key={order.id}>
-								<div className="rounded shadow bg-white">
-									<img
-										src={order.photo ? process.env.PUBLIC_URL + "/images/default_photo.svg" : process.env.PUBLIC_URL + "/images/default_photo.svg"}
-										className="w-full" />
-									<div className="pb-4 px-4">
-										<div className="pt-4 pb-4">
-											<div className="text-text text-sm">Nama</div>
-											<div className="text-black font-medium text-base pb-2">{order.full_name}</div>
-											<div className="text-text text-sm">Nomor Telepon</div>
-											<div className="text-black font-medium text-base">{order.phone_number}</div>
-										</div>
-										<div className="text-black text-base font-semibold border-border border-b-2">Pengaturan File</div>
-										<div className="flex py-2">
-											<div className="w-1/2">
-												<div className="text-text text-xs uppercase">Halaman</div>
-												<div className="border-border border-2 rounded px-2 inline">
-													{order.pages}
-												</div>
+  console.log(orders)
+  if (props.auth.data) {
+    return (
+      <div className="bg-bg h-screen">
+        <Navbar goTo={"LogOut"}></Navbar>
+        <div className="sm:pt-32 pt-40 px-8">
+          <div className="text-3xl ml-2">Order</div>
+        </div>
+        <div className="flex flex-wrap px-8">
+          {orders && orders.map(order => {
+            return (
+              <div className="w-full sm:w-1/2 md:w-1/3 p-1" key={order.id}>
+                <div className="rounded shadow bg-white">
+                  <img
+                    src={order.photo ? process.env.PUBLIC_URL + "/images/default_photo.svg" : process.env.PUBLIC_URL + "/images/default_photo.svg"}
+                    className="w-full" />
+                  <div className="pb-4 px-4">
+                    <div className="pt-4 pb-4">
+                      <div className="text-text text-sm">Nama</div>
+                      <div className="text-black font-medium text-base pb-2">{order.full_name}</div>
+                      <div className="text-text text-sm">Nomor Telepon</div>
+                      <div className="text-black font-medium text-base">{order.phone_number}</div>
+                    </div>
+                    <div className="text-black text-base font-semibold border-border border-b-2">Pengaturan File</div>
+                    <div className="flex py-2">
+                      <div className="w-1/2">
+                        <div className="text-text text-xs uppercase">Halaman</div>
+                        <div className="border-border border-2 rounded px-2 inline">
+                          {order.pages}
+                        </div>
+                      </div>
+                      <div className="w-1/2">
+                        <div className="text-text text-xs uppercase">Jumlah Rangkap</div>
+                        <div className="border-border border-2 rounded px-2 inline">
+                          {order.copies}
+                        </div>
+                      </div>
+                    </div>
+                    <div className="flex">
+                      <div className="w-1/2 pb-2">
+                        <div className="text-text text-xs uppercase">Product</div>
+                        <div className="border-border border-2 rounded px-2 inline">
+                          {order.product_name}
+                        </div>
+                      </div>
+                      <div className="w-1/2 pb-2">
+                        <div className="text-text text-xs uppercase">File</div>
+                        <div className="border-primary border-2 bg-primary text-white rounded px-2 inline cursor-pointer">
+                          Download
 											</div>
-											<div className="w-1/2">
-												<div className="text-text text-xs uppercase">Jumlah Rangkap</div>
-												<div className="border-border border-2 rounded px-2 inline">
-													{order.copies}
-												</div>
-											</div>
-										</div>
-										<div className="flex">
-											<div className="w-1/2 pb-2">
-												<div className="text-text text-xs uppercase">Product</div>
-												<div className="border-border border-2 rounded px-2 inline">
-													{order.product_name}
-												</div>
-											</div>
-											<div className="w-1/2 pb-2">
-												<div className="text-text text-xs uppercase">File</div>
-												<div className="border-primary border-2 bg-primary text-white rounded px-2 inline cursor-pointer">
-													Download
-											</div>
-											</div>
-										</div>
-									</div>
-									<div className="flex">
-										<button className="w-1/2 bg-accent text-white text-center rounded-bl py-4 px-4">
-											Tolak
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex">
+                    <button className="w-1/2 bg-accent text-white text-center rounded-bl py-4 px-4">
+                      Tolak
 										</button>
-										<button className="w-1/2 bg-primary text-white text-center rounded-br py-4 px-4">
-											Konfirmasi
+                    <button className="w-1/2 bg-primary text-white text-center rounded-br py-4 px-4">
+                      Konfirmasi
 										</button>
-									</div>
-								</div>
-							</div>
-						)
-					}) : "Loading..." : "Tidak ada Order"}
+                  </div>
+                </div>
+              </div>
+            )
+          }) : "Loading..." : "Tidak ada Order"}
 				</div>
-			</div>
-		)
-	} else {
-		return null;
-	}
+      </div>
+    )
+  } else {
+    return null;
+  }
 }
 
 const mapStateToProps = (state) => {
-	return {
-		auth: state.auth,
-		order: state.order
-	}
+  return {
+    auth: state.auth,
+    order: state.order
+  }
 }
 
 const mapDispatchToProps = (dispatch) => {
-	return {
-		getOrders: (partnerId) => { dispatch(getOrders(partnerId)) },
-		resetOrders: () => { dispatch(resetOrders()) }
-	}
+  return {
+    getOrders: (partnerId) => { dispatch(getOrders(partnerId)) },
+    resetOrders: () => { dispatch(resetOrders()) }
+  }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Order)
