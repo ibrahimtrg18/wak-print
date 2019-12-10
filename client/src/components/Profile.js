6bd988e04fa091522419a6c131c642538fb93afe
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2019-12-10 15:14:28 +0700

    fix profile

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -5,12 +5,14 @@ import Navbar from './Navbar';
 import { getProfile, resetProfile } from "../redux/actions/profileActions";
 
 const Profile = (props) => {
-
-  const [profile, setProfile] = useState(null);
+  const [profile, setProfile] = useState(props.profile.data);
 
   useEffect(() => {
     document.title = "Profile"
     props.getProfile(props.auth.data.id)
+    return () => {
+      props.resetProfile()
+    }
   }, [])
 
   useEffect(() => {
@@ -32,60 +34,66 @@ const Profile = (props) => {
         <div className="sm:pt-32 pt-40 px-8">
           <h1 className="text-3xl ml-2">Profile</h1>
           <div className="inline px-8">
-            {props.profile.isLoading ? "Loading" : profile && profile.info.photo ?
-              <img
-                src={process.env.PUBLIC_URL + "/images/home.svg"}
-                className="mx-auto" />
-              : <img
-                src={process.env.PUBLIC_URL + "/images/default_photo.svg"}
-                className="mx-auto" />
-            }
-            <Link to="#" className="text-primary text-semibold float-right">Edit Profile</Link>
-            <h1 className="text-black text-base font-semibold border-border border-b-2">Info</h1>
-            {props.profile.isLoading ? "Loading" : profile && profile ?
+            {props.profile.isLoading ?
+              "Loading" :
               <>
-                <p className="text-base text-text">Name Percetakan</p>
-                <p className="text-base text-black">{profile.info.business_name}</p>
-                <p className="text-base text-text">Name Lengkap</p>
-                <p className="text-base text-black">{profile.info.full_name}</p>
-                <p className="text-base text-text">Deskripsi</p>
-                <p className="text-base text-black">{profile.info.description}</p>
-                <p className="text-base text-text">Alamat</p>
-                <p className="text-base text-black">{profile.info.address}</p>
-              </>
-              : "Not found"}
-            <h1 className="text-black text-base font-semibold border-border border-b-2 mt-4">Status</h1>
-
-            <h1 className="text-black text-base font-semibold border-border border-b-2 mt-4">Jasa</h1>
-            {props.profile.isLoading ? "Loading" : profile && profile.products ?
-              <table className="table-fixed">
-                <thead>
-                  <tr>
-                    <th className="w-1/12 px-4 py-2">No.</th>
-                    <th className="w-9/12 px-4 py-2">Nama</th>
-                    <th className="w-1/12 px-4 py-2">Nominal</th>
-                    <th className="w-1/12 px-4 py-2">Action</th>
-                  </tr>
-                </thead>
-                {profile.products.map((product, index) => {
-                  return (
-                    <tbody>
-                      <tr className={index % 2 == 1 ? "bg-border" : null}>
-                        <td className="border px-4 py-2 border-text">{index}</td>
-                        <td className="border px-4 py-2 border-text">{product.name}</td>
-                        <td className="border px-4 py-2 border-text">{product.price}</td>
-                        <td className="border px-4 py-2 border-text">
-                          <div className="flex">
-                            <button className="bg-primary rounded text-white px-4 py-2 mx-2">Edit</button>
-                            <button className="bg-danger rounded text-white px-4 py-2">Hapus</button>
-                          </div>
-                        </td>
+                {profile && profile.info.photo ?
+                  <img
+                    src={process.env.PUBLIC_URL + "/images/home.svg"}
+                    className="mx-auto" />
+                  : <img
+                    src={process.env.PUBLIC_URL + "/images/default_photo.svg"}
+                    className="mx-auto" />
+                }
+                <Link to="#" className="text-primary text-semibold float-right">Edit Profile</Link>
+                <h1 className="text-black text-base font-semibold border-border border-b-2">Info</h1>
+                {profile && profile ?
+                  <>
+                    <p className="text-base text-text">Name Percetakan</p>
+                    <p className="text-base text-black">{profile.info.business_name}</p>
+                    <p className="text-base text-text">Name Lengkap</p>
+                    <p className="text-base text-black">{profile.info.full_name}</p>
+                    <p className="text-base text-text">Deskripsi</p>
+                    <p className="text-base text-black">{profile.info.description}</p>
+                    <p className="text-base text-text">Alamat</p>
+                    <p className="text-base text-black">{profile.info.address}</p>
+                  </>
+                  : "Not found"}
+                <h1 className="text-black text-base font-semibold border-border border-b-2 my-4">Status</h1>
+                <Link to="#" className="text-primary text-semibold float-right">Tambah Jasa</Link>
+                <h1 className="text-black text-base font-semibold border-border border-b-2">Jasa</h1>
+                {profile && profile.products ?
+                  <table className="table-fixed">
+                    <thead>
+                      <tr>
+                        <th className="w-1/12 px-4 py-2">No.</th>
+                        <th className="w-9/12 px-4 py-2">Nama</th>
+                        <th className="w-1/12 px-4 py-2">Nominal</th>
+                        <th className="w-1/12 px-4 py-2">Action</th>
                       </tr>
-                    </tbody>
-                  )
-                })}
-              </table>
-              : "Tidak ada product"}
+                    </thead>
+                    {profile.products.map((product, index) => {
+                      return (
+                        <tbody>
+                          <tr className={index % 2 == 1 ? "bg-border" : null}>
+                            <td className="border px-4 py-2 border-text">{index}</td>
+                            <td className="border px-4 py-2 border-text">{product.name}</td>
+                            <td className="border px-4 py-2 border-text">{product.price}</td>
+                            <td className="border px-4 py-2 border-text">
+                              <div className="flex">
+                                <button className="bg-primary rounded text-white px-4 py-2 mx-2">Edit</button>
+                                <button className="bg-danger rounded text-white px-4 py-2">Hapus</button>
+                              </div>
+                            </td>
+                          </tr>
+                        </tbody>
+                      )
+                    })}
+                  </table>
+                  : "Tidak ada product"}
+              </>
+            }
+
           </div>
         </div>
       </div >
