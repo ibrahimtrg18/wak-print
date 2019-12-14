c116e89c2f8d1ee2cee3d5b24c620d4d0e9000ef
Ibrahim <ibrahimtarigan@gmail.com>
2019-12-14 21:00:57 +0700

    delete history and file route

--- server/app.js
+++ server/app.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 router.use("/", require("./routes/home")); // home and search
-router.use("/file", require("./routes/file")); // files
+// router.use("/file", require("./routes/file")); // files
 // router.use("/photo", require("./routes/photo")); // photo / foto 
 router.use("/order", require("./routes/order")); // order / pesanan
-router.use("/history", require("./routes/history")); // history / riwayat
+// router.use("/history", require("./routes/history")); // history / riwayat
 router.use("/balance", require("./routes/balance")); // balance / wallet / saldo
 router.use("/user", require("./routes/user")); // user
 router.use("/partner", require("./routes/partner")); // wakprint
