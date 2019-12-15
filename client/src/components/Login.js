d479022c5df726ea135653cfff2efc4cf3d9729e
Ibrahim <ibrahimtarigan@gmail.com>
2019-12-15 13:48:07 +0700

    change method

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -28,16 +28,16 @@ const Login = (props) => {
     setMessage(props.auth.message)
   }, [props.auth.message])
 
-  const _goToRegister = () => {
+  const goToRegister = () => {
     props.history.push("/register")
   }
 
-  const _handleChange = (event) => {
-    const {name,value} = event.target
+  const handleChange = (event) => {
+    const { name, value } = event.target
     setValues({ ...values, [name]: value })
   }
 
-  const _handleSubmit = async (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault()
     props.login(values)
   }
@@ -46,7 +46,7 @@ const Login = (props) => {
     <div className="bg-bg" style={{ height: "100%", minHeight: "100vh" }}>
       <Navbar
         goTo="Daftar Disini"
-        goToRedirect={() => _goToRegister()} />
+        goToRedirect={() => goToRegister()} />
       <div className="sm:flex sm:pt-32 pt-32">
         <div className="sm:w-1/2 px-4 sm:block md:block hidden">
           <img
@@ -59,7 +59,7 @@ const Login = (props) => {
             Masuk Mitra WakPrint
           </div>
           <p className="text-base font-medium text-danger text-center">{message}</p>
-          <form className="items-center" onSubmit={(event) => _handleSubmit(event)}>
+          <form className="items-center" onSubmit={(event) => handleSubmit(event)}>
             <label className="block text-md uppercase font-base text-text py-2">
               E-Mail
               <input
@@ -69,8 +69,9 @@ const Login = (props) => {
                 ref={emailRef}
                 value={values.email}
                 placeholder="example@example.com"
-                onChange={(event) => _handleChange(event)}
-                className="w-full border-primary border-2 rounded-lg py-2 px-3 focus:shadow-outline placeholder-secondary" />
+                onChange={(event) => handleChange(event)}
+                className="w-full border-primary border-2 rounded-lg py-2 px-3 focus:shadow-outline placeholder-secondary"
+                required />
             </label>
             <label
               className="block text-md uppercase font-base text-text py-2">
@@ -81,22 +82,23 @@ const Login = (props) => {
                 id="password"
                 value={values.password}
                 placeholder="******"
-                onChange={(event) => _handleChange(event)}
-                className="w-full border-primary border-2 rounded-lg py-2 px-3 focus:shadow-outline placeholder-secondary" />
+                onChange={(event) => handleChange(event)}
+                className="w-full border-primary border-2 rounded-lg py-2 px-3 focus:shadow-outline placeholder-secondary"
+                required />
             </label>
             <div className="flex text-xs font-base text-gray-800 justify-center my-2">
               <p>Belum punya akun?</p>
               <div
-                onClick={() => _goToRegister()}
+                onClick={() => goToRegister()}
                 className="ml-1 text-primary cursor-pointer">Daftar</div>
             </div>
             <input
               type="submit"
               value={props.auth.isLoading && props.auth.isLoading ? "Loading..." : "masuk"}
-              className={props.auth.isLoading && props.auth.isLoading ? 
-                "rounded bg-secondary text-white py-2 px-4 uppercase text-lg text-medium w-full focus:shadow-outline cursor-not-allowed" 
-              : 
-              "rounded bg-primary text-white py-2 px-4 uppercase text-lg text-medium w-full focus:shadow-outline cursor-pointer hover:bg-secondary"} />
+              className={props.auth.isLoading && props.auth.isLoading ?
+                "rounded bg-secondary text-white py-2 px-4 uppercase text-lg text-medium w-full focus:shadow-outline cursor-not-allowed"
+                :
+                "rounded bg-primary text-white py-2 px-4 uppercase text-lg text-medium w-full focus:shadow-outline cursor-pointer hover:bg-secondary"} />
           </form>
         </div>
       </div>
