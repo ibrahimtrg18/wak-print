76e9475e5098674cb4f0beee6ba6261c459c3a80
Ibrahim <ibrahimtarigan@gmail.com>
2019-12-15 13:47:40 +0700

    add 404

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 
 import Login from './components/Login'
 import Register from './components/Register';
@@ -7,6 +7,7 @@ import Home from './components/Home';
 import Orders from './components/Orders';
 import Profile from './components/Profile';
 import History from './components/History';
+import Edit from './components/Edit';
 
 function App() {
   return (
@@ -15,11 +16,13 @@ function App() {
         <Switch>
           <Route path="/login" component={Login}></Route>
           <Route path="/register" component={Register}></Route>
+          <Route path="/profile/edit" component={Edit}></Route>
           <Route path="/profile" component={Profile}></Route>
           <Route path="/history" component={History}></Route>
           <Route path="/order" component={Orders}></Route>
           <Route path="/home" component={Home}></Route>
           <Route path="/" exact component={Home}></Route>
+          <Redirect from="*" to="/"></Redirect>
         </Switch>
       </div>
     </Router>
